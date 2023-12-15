0e6f47acc41e5ac32d9ce9751190e5458226f72e
AndyNichol3 <125906484+AndyNichol3@users.noreply.github.com>
2023-12-15 19:23:58 +0000

    more testing to see if this works

--- .ccls-cache/@home@runner@MineSweeperC/main.cpp
+++ .ccls-cache/@home@runner@MineSweeperC/main.cpp
@@ -2,6 +2,10 @@
 
 int main() {
   // adding an alteration to test if git works 
+
+  std::cout << "Hello World!\n";
+
+  // making code alterations just to test
   
   std::cout << "Hello World!\n";
 }
